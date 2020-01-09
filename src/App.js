d11eae92164f1aa656588c5b946c1375d20aa319
Will Carter <will.carter@fergusllc.com>
2020-01-09 13:57:47 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export const App = () => {
+  const [rangeValue, setRangeValue] = useState(0)
+  
+  const reset = (e) => {
+    e.preventDefault()
+    setRangeValue(0)//reset state to 0 in App
+    //how can I get the initial Position in Range component back to 0?
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>The range value is: {rangeValue}</p>
+      <Range setRangeValue = {setRangeValue}/>
+      <p><a href="/#" onClick={reset}>reset</a></p>
     </div>
-  );
+  )
+}
+
+const Range = ({setRangeValue: setRangeValue}) =>
+{
+  const handleChange = (event) => {
+    setRangeValue(event.target.value)
+  }
+  return (
+    <input type="range" min="0" max="10" step="1" defaultValue={0} onChange={(event) => {handleChange(event)}} />
+  )
 }
 
 export default App;
